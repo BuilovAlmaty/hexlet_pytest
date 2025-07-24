df7dfb02a8a0b1f166b5aaade716e6815e4892c6
Alexey Builov <builovalmaty@gmail.com>
2025-07-24 21:44:20 +0500

    add new check for reverse()

--- hexlet_pytest/example.py
+++ hexlet_pytest/example.py
@@ -1,3 +1,4 @@
 # Эта функция переворачивает переданную строку
 def reverse(string):
     return string[::-1]
+reverse("sdd")
--- tests/test_example.py
+++ tests/test_example.py
@@ -1,5 +1,5 @@
 from hexlet_pytest.example import reverse
-
+from pathlib import Path
 
 def test_reverse():
     assert reverse("Hexlet") == "telxeH"
@@ -7,3 +7,16 @@ def test_reverse():
 
 def test_reverse_for_empty_string():
     assert reverse("") == ""
+
+def get_path(file_name):
+    return Path(__file__).parent / "test_data" / file_name
+
+def test_progress():
+    text_in = get_path("test_reverse_before.txt").read_text()
+    text_out = get_path("test_reverse_after.txt").read_text()
+
+    actual = reverse(text_in)
+
+    assert actual.strip() == text_out.strip()
+ 
+
